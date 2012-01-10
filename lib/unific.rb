5f9c0c0b7264baa8d63964a438f4cf66b169c1ed
Jim Wise <jwise@draga.com>
2012-01-10 16:10:34 -0500

    Move unification implementation here from rulog, along with tests.

--- lib/unific.rb
+++ lib/unific.rb
@@ -1,3 +1,162 @@
+require 'singleton'
+
 class Unific
-  VERSION = '1.0.0'
+  VERSION = '0.9.0'
+
+  class Env
+    @@trace = 0
+
+    def initialize prev = {}
+      @theta = prev.clone
+    end
+
+    def self.trace lvl
+      if lvl
+        @@trace = lvl
+      else
+        @@trace = @@trace + 1
+      end
+    end
+
+    def self.untrace
+      @@trace = 0
+    end
+
+    def fresh? x
+      not @theta.has_key? x
+    end
+
+    def extend mappings
+      Env.new @theta.update mappings.reject {|k, v| k.kind_of? Wildcard or v.kind_of? Wildcard }
+    end
+
+    def to_s
+      "{ " + @theta.map{|k, v| "#{k} => #{v}"}.join(", ") + "} "
+    end
+
+    # returns either `false' or the MGU of the two terms, which can be
+    #    a.) vars
+    #    b.) wildcards
+    #    c.) any ruby Enumerable, in which case unification recurs on the members
+    #    d.) any other ruby object (as a ground term)
+    #
+    # this is a functional interface -- a new env is returned with the MGU, as taken
+    # against the bindings already in this env
+    def unify a, b
+      puts "unifying #{a.to_s} and #{b.to_s}" if @@trace > 0
+
+      # if either is already bound, substitute up front
+      a = instantiate a
+      b = instantiate b
+
+      # any remaining Var is fresh.
+      if a.kind_of? Var and b.kind_of? Var
+          extend a => b
+      elsif a.kind_of? Var
+          extend a => b
+      elsif b.kind_of? Var
+          extend b => a
+      elsif a.kind_of? Enumerable and b.kind_of? Enumerable
+        return Unific::fail unless a.size == b.size
+        a.zip(b).inject(self) do |e, pair|
+          e.unify(pair[0], pair[1]) or return Unific::fail
+        end
+      else # both are ground terms
+        if a == b
+          self
+        else
+          Unific::fail
+        end
+      end
+    end
+
+    # substitute any bound variables in an arbitrary expression, using traversal rules of traverse
+    def instantiate s
+      _traverse s do |v|
+        if fresh? v
+          v
+        else
+          instantiate @theta[v]
+        end
+      end
+    end
+
+    # alpha-rename an expression (all variables get new variables of same name.  useful, e.g. to give
+    # each Rule its own private copy of all of its variables.
+    def rename s
+      _traverse s do |v|
+        if fresh? v
+          n = Unific::Var.new(v.name)
+          @theta[v] = n;
+          n
+        else
+          instantiate @theta[v]
+        end
+      end
+    end
+
+    # helper for instantiate and rename
+    # given an argument, if it is an:
+    #   a.) var, replace it with the result of calling a block on it
+    #   b.) enumerable, recur, instantiating it's members
+    #   c.) any object, return it
+    def _traverse s, &block
+      case s
+      when Unific::Wildcard
+        s
+      when Var
+        block.call(s)
+      # XXX XXX rulog had handling for Functor here, but it should work as enumerable now
+      when String
+        # in ruby 1.8, strings are enumerable, but we want them to be ground
+        s
+      when Enumerable
+        s.map {|x| _traverse(x, &block)}
+      else
+        s
+      end
+    end
+  end
+
+  def self.unify a, b, env = Env.new
+    env.unify a, b
+  end
+
+  class Var
+    attr_accessor :name
+
+    def initialize name = "new_var"
+      @name = name
+      self.freeze
+    end
+    
+    def to_s
+      "?#{@name}"
+    end
+  end
+
+  class Wildcard < Var
+    include Singleton
+
+    def initialize
+      super "_"
+    end
+
+    def to_s
+      "_"
+    end
+
+    def == x
+      true
+    end
+  end
+
+  def self._
+    Unific::Wildcard.instance
+  end
+
+  def self.fail
+    false
+  end
+
 end
--- test/test_unific.rb
+++ test/test_unific.rb
@@ -2,7 +2,42 @@ require "test/unit"
 require "unific"
 
 class TestUnific < Test::Unit::TestCase
-  def test_sanity
-    flunk "write tests or I will kneecap you"
+
+  def test_unify_simple
+    assert Unific::unify(42, 42)
+    assert Unific::unify(42, Unific::Var.new)
+    assert Unific::unify(Unific::Var.new, 42)
+    assert Unific::unify(Unific::Var.new, Unific::Var.new)
+    v1 = Unific::Var.new("v1")
+    assert Unific::unify(v1, v1)
+    assert Unific::unify(v1, 42).unify(v1, 42)
+    assert !Unific::unify(v1, 42).unify(v1, 35)
   end
+
+  def test_unify_enum
+    assert Unific::unify([1, 2, 3], [1, 2, 3])
+    assert !Unific::unify([1, 2, 3], [1, 2, 3, 4])
+    assert Unific::unify([1, 2, 3], [1, Unific::Var.new, 3])
+  end
+
+  def test_wildcard
+    assert Unific::unify(Unific::_, 42);
+    assert Unific::unify(Unific::_, Unific::Var.new);
+    v1 = Unific::Var.new("v1")
+    e1 = Unific::Env.new
+    e2 = Unific::unify(v1, 42);
+    assert Unific::unify(v1, Unific::_, e2);
+    assert Unific::unify([1, 2, 3], Unific::_)
+    assert Unific::unify([1, Unific::_, 3], [1, 2, 3])
+  end
+
+  def test_vars
+    v1 = Unific::Var.new("v1")
+    v2 = Unific::Var.new("v2")
+    assert Unific::unify(v1, v2).unify(v1, 42).unify(v2, 42)
+    assert !Unific::unify(v1, v2).unify(v1, 42).unify(v2, 35)
+    assert Unific::unify(v1, 42).unify(v1, v2).unify(v2, 42)
+    assert !Unific::unify(v1, 42).unify(v1, v2).unify(v2, 35)
+  end
+
 end
